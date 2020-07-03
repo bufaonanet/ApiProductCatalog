adb160d2fd3ac5fa454f022f0af2697c1c1d7a9d
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-07-03 18:30:35 -0300

    commit inicial

---
+++ ProductCatalog/Data/Maps/ProdutcMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data.Maps
+{
+    public class ProdutcMap : IEntityTypeConfiguration<Produtc>
+    {
+        public void Configure(EntityTypeBuilder<Produtc> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.DateCreate).IsRequired();
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(1024).HasColumnType("varchar(1024)");
+            builder.Property(p => p.Image).IsRequired().HasMaxLength(1024).HasColumnType("varchar(1024)");
+            builder.Property(p => p.Image).IsRequired().HasMaxLength(1024).HasColumnType("varchar(1024)");
+            builder.Property(p => p.LastUpdateDate).IsRequired();
+            builder.Property(p => p.Price).IsRequired().HasColumnType("money");
+            builder.Property(p => p.Quantity).IsRequired();
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(120).HasColumnType("varchar(120)");
+            builder.HasOne(p => p.Category).WithMany(c => c.Produtcs);
+        }
+    }
+}
---
+++ ProductCatalog/Data/StoreDataContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Produtc> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=db_products;User ID=SA; Password=Admin123@");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProdutcMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+        }
+
+
+    }
+
+}
