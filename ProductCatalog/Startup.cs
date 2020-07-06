2fbf65017a32b168a725f232f326c716e82229a1
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-07-06 17:58:33 -0300

    Adicionando documentação com Swagger

--- ProductCatalog/Startup.cs
+++ ProductCatalog/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using ProductCatalog.Data;
 using ProductCatalog.Repositoreis;
-using ZNetCS.AspNetCore.Compression.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.Swagger;
 
 namespace ProductCatalog
 {
@@ -15,6 +17,9 @@ namespace ProductCatalog
         {
             services.AddMvc();
 
+            //adicionando o middleware para o serviço de compressão
+            services.AddResponseCompression();
+
             //Objetos de escopo (AddScoped) são os mesmos em uma solicitação, 
             //mas diferentes entre solicitações diferentes.;
             services.AddScoped<StoreDataContext, StoreDataContext>();
@@ -24,15 +29,18 @@ namespace ProductCatalog
             services.AddTransient<ProductRepository, ProductRepository>();
             services.AddTransient<CategoryRepository, CategoryRepository>();
 
-            //adicionando o middleware para o serviço de compressão
-            services.AddResponseCompression();
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "My Api Product Category", Version = "v1" });
+            });
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
-           
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -41,6 +49,12 @@ namespace ProductCatalog
             });
 
             app.UseResponseCompression();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "my api - v1");
+            });
 
 
         }
